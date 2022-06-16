1dcf73ef2dbc6d7534295c40520dda7e96ec3152
Keijaoh Campbell <keijaoh@gmail.com>
2022-06-16 11:03:15 -0400

    Initial Commit

---
+++ IdexxInterviewRedesign/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  IdexxInterviewRedesign
+//
+//  Created by Keijaoh Campbell on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IdexxInterviewRedesign/IdexxInterviewRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdexxInterviewRedesignApp.swift
+//  IdexxInterviewRedesign
+//
+//  Created by Keijaoh Campbell on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct IdexxInterviewRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
